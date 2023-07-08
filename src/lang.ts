05d8425c8dc173125d1ceaeaf7879d10c5f1d9df
yakisova41 <yakisoba5emon@gmail.com>
2023-07-08 19:56:10 +0000

    🌐 言語の追加

--- src/lang.ts
+++ src/lang.ts
@@ -1,24 +1,49 @@
 const langList: Record<string, Record<string, string>> = {
   All: {
     ja: "すべて",
+    de: "Alle",
+    es: "Todo",
+    fr: "Tous",
+    ko: "전체",
+    zh: "全部",
   },
   Images: {
     ja: "画像",
+    de: "Bilder",
+    es: "Imágenes",
+    ko: "이미지",
+    zh: "图片",
   },
   Videos: {
     ja: "動画",
+    fr: "Vidéos",
+    ko: "동영상",
+    zh: "视频",
   },
   Maps: {
     ja: "地図",
+    ko: "지도",
+    zh: "地图",
   },
   News: {
     ja: "ニュース",
+    es: "Noticias",
+    fr: "Actualités",
+    ko: "뉴스",
+    zh: "新闻",
   },
   Shopping: {
     ja: "ショッピング",
+    ko: "쇼핑",
+    zh: "购物",
   },
   Books: {
     ja: "書籍",
+    de: "Bücher",
+    es: "Libros",
+    fr: "Livres",
+    ko: "쇼핑",
+    zh: "图书",
   },
 };
 
