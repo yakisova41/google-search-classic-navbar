edb95b83da901ec18d5952161bbb8e5db986acf0
yakisova41 <yakisoba5emon@gmail.com>
2023-09-09 20:45:47 +0000

    ✨ add books shoping enable icon

--- src/registerNavItems.ts
+++ src/registerNavItems.ts
@@ -98,7 +98,7 @@ export default function registerNavItems(itemsDom: NavBarItems) {
     ),
     itemsDom.createItemElem(
       translate("Books"),
-      getIconSvgElement(paths.bks),
+      getIconSvgElement(enablePaths.bks),
       false
     )
   );
